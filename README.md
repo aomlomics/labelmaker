fab03cd2e6f112c8de2ec50dc4c90e0dc736d785
Luke Thompson <lukethompson@gmail.com>
2018-09-29 13:44:10 -0700

    command-line version replaces ipython notebook

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
-<img src="https://images-na.ssl-images-amazon.com/images/I/61UOa%2BgxXuL._SL1024_.jpg" height=200> <img src="https://assets.fishersci.com/TFS-Assets/CCG/product-images/F144079~p.eps-650.jpg" height=200>
+<img src="https://images-na.ssl-images-amazon.com/images/I/61UOa%2BgxXuL._SL1024_.jpg" height=150> <img src="example.png" height=150> <img src="https://assets.fishersci.com/TFS-Assets/CCG/product-images/F144079~p.eps-650.jpg" height=150>
 
 # Labelmaker
 
-Make printable QR code labels for samples using basic information about a project (later: information provided in a spreadsheet).
+Make printable QR code labels for samples using basic information about a project. Currently the sample names iterate from 1 to N, where N is the number of samples. In a future release the sample names can be provided by a spreadsheet.
 
 ## Installation
 
-First create a conda environment called `labels` where you will install the package [`qrcode`](https://pypi.org/project/qrcode/).
+If you don't have Conda installed on your machine, install [Miniconda](https://conda.io/miniconda.html) for your operating system (Python 3.7+ version).
+
+Create a conda environment called `labels` where you will install the package [`qrcode`](https://pypi.org/project/qrcode/).
 
 ```
-conda create -n labels pandas python=3
+conda create -n labels pandas click python=3
 source activate labels
 pip install qrcode[pil]
 ```
@@ -18,8 +20,22 @@ Clone the Labelmaker repository to your computer:
 
 ```
 git clone https://github.com/cuttlefishh/labelmaker
+cd labelmaker
 ```
 
 ## Execution
 
-Run the Jupyter notebook. This will be replaced by a Python script in the near future.
+Just run the command `generate_labels.py` with the required parameters:
+
+```
+./generate_labels.py \
+  --project MyProject \
+  --contact MySurname \
+  --date 20180929 \
+  --num_samples 5 \
+  --num_replicates 2 \
+  --label_width 1.05 \
+  --label_height 0.5
+```
+
+To learn more about each parameter, run `./generate_labels.py --help`.
---
+++ generate_labels.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import click
+import os
+import numpy as np
+import pandas as pd
+import qrcode
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw 
+
+def create_directory(project):
+
+    # create directory for project labels
+    newdir = 'labels_%s' % project
+    if not os.path.exists(newdir):
+        os.makedirs(newdir)
+
+def make_label(project, contact, date, sample, replicate, label_width, label_height):
+
+    # generate text code and qr code
+    code = '%s.%s.%s.s%03d.r%02d' % (project, contact, date, sample, replicate)
+    string = 'Project: %s\nContact: %s\nDate: %s\nSample: s%03d\nReplicate: r%02d' % (
+        project, contact, date, sample, replicate)
+
+    # make qr code
+    qr = qrcode.QRCode(
+        #version=1, # set fit=True below to make this automatic
+        #error_correction=qrcode.constants.ERROR_CORRECT_L, # default is ERROR_CORRECT_M
+        box_size=8, # number of pixels per box
+        border=10, # larger border yields smaller qr code
+    )
+    qr.add_data(code)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    # make label from qr code and string
+    label = Image.new('RGB', (int(img.height*label_width/label_height), img.height), color='white')
+    label.paste(img, (0,0))
+    draw = ImageDraw.Draw(label)
+    font = ImageFont.truetype('Monaco.dfont', 24)
+    draw.text((img.height,int(img.height*0.3)), string, (0,0,0), font=font)
+    label.save('labels_%s/label_s%03d_r%02d.png' % (project, sample, replicate))
+
+@click.command()
+@click.option('--project', '-p', required=True, type=str,
+              help="Short project name. Must not contain spaces.")
+@click.option('--contact', '-c', required=True, type=str,
+              help="Last name of point of contact. Must not contain spaces.")
+@click.option('--date', '-d', required=True, type=int,
+              help="Date as YYYYMMDD or YYYYMM.")
+@click.option('--num_samples', '-s', required=True, type=int,
+              help="Number of unique samples.")
+@click.option('--num_replicates', '-r', required=True, type=int,
+              help="Number of replicates per sample.")
+@click.option('--label_width', '-w', required=True, type=float,
+              help="Width of label in inches.")
+@click.option('--label_height', '-h', required=True, type=float,
+              help="Height of label in inches.")
+
+def main(project, contact, date, num_samples, num_replicates, label_width, label_height):
+
+    create_directory(project)
+
+    # make labels, iterating over sample numbers and replicates
+    for sample in np.arange(num_samples)+1:
+        for replicate in np.arange(num_replicates)+1:
+            make_label(project, contact, date, sample, replicate, label_width, label_height)
+
+if __name__ == "__main__":
+    main()
