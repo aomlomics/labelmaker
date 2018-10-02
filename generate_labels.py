3f641c2669673aa831c82c4f9aae5a1c55df27df
Luke Thompson <lukethompson@gmail.com>
2018-10-01 20:56:31 -0700

    adding blank next to sample ID

--- generate_labels.py
+++ generate_labels.py
@@ -20,15 +20,15 @@ def make_label(project, contact, sample_type, date, sample, replicate, label_wid
 
     # generate text code and qr code
     code = '%s_%s_%s_%s_s%03d_r%02d' % (project, contact, sample_type, date, sample, replicate)
-    string = 'Project: %s\nContact: %s\nType: %s\nDate: %s\nSample ID: s%03d\nReplicate: r%02d' % (
+    string = 'Project:%s\nContact:%s\nType:%s\nDate:%s\nSampleID:s%03d ________\nReplicate:r%02d' % (
         project, contact, sample_type, date, sample, replicate)
 
     # make qr code
     qr = qrcode.QRCode(
         #version=1, # set fit=True below to make this automatic
         #error_correction=qrcode.constants.ERROR_CORRECT_L, # default is ERROR_CORRECT_M
-        box_size=8, # number of pixels per box
-        border=10, # larger border yields smaller qr code
+        box_size=6, # number of pixels per box
+        border=12, # larger border yields smaller qr code
     )
     qr.add_data(code)
     qr.make(fit=True)
@@ -39,7 +39,7 @@ def make_label(project, contact, sample_type, date, sample, replicate, label_wid
     label.paste(img, (0,0))
     draw = ImageDraw.Draw(label)
     font = ImageFont.truetype('Monaco.dfont', 32)
-    draw.text(((img.height*0.9), int(img.height*0.25)), string, (0,0,0), font=font)
+    draw.text(((img.height*0.85), int(img.height*0.18)), string, (0,0,0), font=font)
     label.save('labels_%s/label_s%03d_r%02d.png' % (project, sample, replicate))
 
 @click.command()
