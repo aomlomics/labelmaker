48042688d4c3ce1180dd4f01b1143a1880f40d4e
Luke Thompson <lukethompson@gmail.com>
2018-10-17 20:28:01 -0700

    adding support for sample names and lualatex sample sheet

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
-<img src="https://images-na.ssl-images-amazon.com/images/I/61UOa%2BgxXuL._SL1024_.jpg" height=150> <img src="example.png" height=150> <img src="https://assets.fishersci.com/TFS-Assets/CCG/product-images/F144079~p.eps-650.jpg" height=150>
+<img src="example.png" height=150> <img src="https://images-na.ssl-images-amazon.com/images/I/41%2BEgB57iTL.jpg" height=150>
 
 # Labelmaker
 
-Make printable QR code labels for samples using basic information about a project. The sample names iterate from 1 to N, where N is the number of samples; the replicate numbers iterate from 1 to M, where M is the number of replicates per sample. We are planning for a future release where the sample names can be provided in a spreadsheet.
+Make printable QR code labels for samples using basic information about a project. 
 
 ## Installation
+
+Labelmaker has the following dependencies (installation instructions below):
+
+* Conda
+* Tex Live
+
+### Conda
 
 If you don't have Conda installed on your machine, install [Miniconda](https://conda.io/miniconda.html) for your operating system (Python 3.7+ version).
 
@@ -16,6 +23,12 @@ source activate labels
 pip install qrcode[pil]
 ```
 
+### TeX Live
+
+Tex Live provides a comprehensive TeX system with binaries for macOS, Linus, and Windows; you can download it [here](https://tug.org/texlive/). We will use `lualatex`. A TeX installation is not absolutely required, but it is required to render a label sheet (PDF) from the individual label image files (PNG).
+
+### Labelmaker
+
 Clone the Labelmaker repository to your computer:
 
 ```
@@ -25,18 +38,60 @@ cd labelmaker
 
 ## Execution
 
-Just run the command `generate_labels.py` with the required parameters, for example:
+Make a sheet of labels in three easy steps:
+
+1. Generate the labels as PNG files.
+2. Generate a composite sheet of labels as a PDF file.
+3. Print!
+
+### Step 1: Generate individual labels (PNG)
+
+Run the command `generate_labels.py` with the required parameters. To learn more about each parameter, run `./generate_labels.py --help`. Two options for sample names are available:
+
+#### Option 1: Provide a list of samples
+
+If a list of sample names is provided, those sample names are used, and replicate labels are produced from 1 to *N*, where *N* is the specified number of replicates per sample.
 
 ```
 ./generate_labels.py \
-  --project MyProject \
+  --project Project1 \
   --contact MySurname \
   --sample_type DNA_0.2um \
-  --date 20180929 \
-  --num_samples 5 \
-  --num_replicates 2 \
-  --label_width 1.05 \
-  --label_height 0.5
+  --date 180929 \
+  --sample_list sample_list.txt \
+  --num_replicates 2
 ```
 
-To learn more about each parameter, run `./generate_labels.py --help`.
+#### Option 2: Number sequentially
+
+If no list of sample names is provided, samples are named from 1 to *M*, where *M* is the specified number of samples, and replicate labels are produced from 1 to *N*, where *N* is the specified number of replicates per sample.
+
+```
+./generate_labels.py \
+  --project Project2 \
+  --contact MySurname \
+  --sample_type DNA_0.2um \
+  --date 180929 \
+  --num_samples 5 \
+  --num_replicates 2
+```
+
+### Step 2: Generate label sheet (PDF)
+
+Labelmaker produces label sheets intended to be printed on Cryo-Babies labels with part number LCRY-1700 ([Diversified Biotech](https://www.divbio.com/product/lcry-1700)). These are 1.28-inch x 0.5-inch labels in a grid of 5 x 17 labels. 
+
+*[Note: Although the physical labels are 1.28 x 0.5 inches, the Python code and TeX code are calibrated for labels with size 1.05 x 0.5 inches or an aspect ratio of 2.1:1 (width:height). By default, `generate_labels.py` will produce PNG files with dimensions 1.05 x 0.5 inches. The user should not change these defaults if they intend to use the commands below to generate label sheets.]*
+
+Run `lualatex` from the directory containing the PNG files and TEX file using the commands below.
+
+```
+cd /PATH/TO/labelmaker/labels_Project1
+lualatex labelsheet_Project1_LCRY1700.tex
+
+cd /PATH/TO/labelmaker/labels_Project2
+lualatex labelsheet_Project2_LCRY1700.tex
+```
+
+### Step 3: Print
+
+Now just open up the resulting PDF file (e.g., `labelsheet_Project1_LCRY1700.pdf`) and print on a sheet of Cryo-Babies LCRY-1700 labels.
--- generate_labels.py
+++ generate_labels.py
@@ -9,6 +9,10 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw 
 
+template_head = 'labelsheet_template_LCRY1700_head.tex'
+template_tail = 'labelsheet_template_LCRY1700_tail.tex'
+template_cols = 5
+
 def create_directory(project):
 
     # create directory for project labels
@@ -19,16 +23,17 @@ def create_directory(project):
 def make_label(project, contact, sample_type, date, sample, replicate, label_width, label_height):
 
     # generate text code and qr code
-    code = '%s_%s_%s_%s_s%03d_r%02d' % (project, contact, sample_type, date, sample, replicate)
-    string = 'Project:%s\nContact:%s\nType:%s\nDate:%s\nSampleID:s%03d ________\nReplicate:r%02d' % (
+    # longcode = '%s_%s_%s_%s_%s_r%01d' % (project, contact, sample_type, date, sample, replicate)
+    code = '%s_%s_r%01d' % (project, sample, replicate)
+    string = 'Project:%s\nContact:%s\nType:%s\nDate:%s\nSampleID:%s\nReplicate:r%01d' % (
         project, contact, sample_type, date, sample, replicate)
 
     # make qr code
     qr = qrcode.QRCode(
         #version=1, # set fit=True below to make this automatic
-        #error_correction=qrcode.constants.ERROR_CORRECT_L, # default is ERROR_CORRECT_M
+        error_correction=qrcode.constants.ERROR_CORRECT_L, # default is ERROR_CORRECT_M
         box_size=6, # number of pixels per box
-        border=12, # larger border yields smaller qr code
+        border=20, # larger border yields smaller qr code
     )
     qr.add_data(code)
     qr.make(fit=True)
@@ -40,7 +45,7 @@ def make_label(project, contact, sample_type, date, sample, replicate, label_wid
     draw = ImageDraw.Draw(label)
     font = ImageFont.truetype('Monaco.dfont', 32)
     draw.text(((img.height*0.85), int(img.height*0.18)), string, (0,0,0), font=font)
-    label.save('labels_%s/label_s%03d_r%02d.png' % (project, sample, replicate))
+    label.save('labels_%s/label_%s_r%01d.png' % (project, sample, replicate))
 
 @click.command()
 @click.option('--project', '-p', required=True, type=str,
@@ -50,24 +55,50 @@ def make_label(project, contact, sample_type, date, sample, replicate, label_wid
 @click.option('--sample_type', '-t', required=True, type=str,
               help="Type of sample (eg DNA.2um). Must not contain spaces.")
 @click.option('--date', '-d', required=True, type=int,
-              help="Date as YYYYMMDD or YYYYMM.")
-@click.option('--num_samples', '-s', required=True, type=int,
-              help="Number of unique samples.")
-@click.option('--num_replicates', '-r', required=True, type=int,
-              help="Number of replicates per sample.")
-@click.option('--label_width', '-w', required=True, type=float,
-              help="Width of label in inches.")
-@click.option('--label_height', '-h', required=True, type=float,
-              help="Height of label in inches.")
+              help="Date as YYMMDD or YYMM.")
+@click.option('--sample_list', '-l', required=False, type=click.Path(exists=True),
+              help="List of samples. If none is provided, samples will be numbered 1 to num_samples.")
+@click.option('--num_samples', '-s', required=False, type=int, default=5,
+              help="Number of unique samples. [default=5]")
+@click.option('--num_replicates', '-r', required=False, type=int, default=1,
+              help="Number of replicates per sample. [default=1]")
+@click.option('--label_width', '-w', required=False, type=float, default=1.05,
+              help="Width of label in inches. 1.05 works for 1.28in labels. [default=1.05]")
+@click.option('--label_height', '-h', required=False, type=float, default=0.5,
+              help="Height of label in inches. 0.5 works for 0.5in labels. [default=0.05]")
 
-def main(project, contact, sample_type, date, num_samples, num_replicates, label_width, label_height):
+def main(project, contact, sample_type, date, sample_list, num_samples, num_replicates, label_width, label_height):
 
     create_directory(project)
 
-    # make labels, iterating over sample numbers and replicates
-    for sample in np.arange(num_samples)+1:
+    # read sample list or number from 1 to num_samples
+    if (sample_list):
+        with open(sample_list, 'r') as f:
+            samples = [line.rstrip() for line in f.readlines()]
+    else:
+        samples = np.arange(num_samples)+1
+
+    # make labels and latex table, iterating over sample numbers and replicates
+    tex_table = ''
+    tex_counter = 0
+    for sample in samples:
         for replicate in np.arange(num_replicates)+1:
             make_label(project, contact, sample_type, date, sample, replicate, label_width, label_height)
+            tex_table += '\\includegraphics[width=\\w]{label_%s_r1} & ' % sample
+            tex_counter += 1
+            if ((tex_counter % template_cols) == 0):
+                tex_table = tex_table[:-2]
+                tex_table += '\\\\\n'
+
+    # make labelsheet latex file
+    with open(template_head, 'r') as f:
+        head = f.read()
+    with open(template_tail, 'r') as f:
+        tail = f.read()
+    with open(f'labels_{project}/labelsheet_{project}_LCRY1700.tex', 'w') as t:
+        t.write(head)
+        t.write(tex_table) 
+        t.write(tail)
 
 if __name__ == "__main__":
     main()
